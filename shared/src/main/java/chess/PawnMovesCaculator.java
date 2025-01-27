f902653f375fc0fc4c174003bf33a461b8c5f2dc
mickbuck <mb2756@byu.edu>
2025-01-27 15:47:00 -0700

    Phase 0: Commit 9

--- shared/src/main/java/chess/PawnMovesCaculator.java
+++ shared/src/main/java/chess/PawnMovesCaculator.java
@@ -11,8 +11,12 @@ public class PawnMovesCaculator implements PieceMovesCaculator {
         int row = startPosition.getRow();
         int col = startPosition.getColumn();
         int Color = 1 ;
+        int start = 2 ;
+        int end = 8 ;
         if(board.getPiece(startPosition).getTeamColor() == ChessGame.TeamColor.BLACK){
             Color = -1;
+            start = 7;
+            end = 1;
         }
         int[][] captures = {{row + Color*1, col + Color*1}, {row + Color*1, col - Color*1}};
         for (int[] capture : captures) {
@@ -27,25 +31,26 @@ public class PawnMovesCaculator implements PieceMovesCaculator {
                 }
             }
         }
-        overallPossibilities.addAll(promotionHelper(capturePossibilities)) ;
+        overallPossibilities.addAll(promotionHelper(capturePossibilities, end)) ;
         ChessPosition current_position = new ChessPosition(row + Color*1, col);
         if(board.getPiece(current_position) == null
                 && current_position.getRow() < 9 && current_position.getColumn() < 9 && current_position.getRow() > 0 && current_position.getColumn() > 0) {
             ChessMove SingleMove = new ChessMove(startPosition, current_position, null);
             forwardPossibilities.add(SingleMove) ;
             current_position = new ChessPosition(row + Color*2, col);
-            if(row == 2 && board.getPiece(current_position) == null && current_position.getRow() < 9 && current_position.getColumn() < 9 && current_position.getRow() > 0 && current_position.getColumn() > 0){
+            if(row == start && current_position.getRow() < 9 && current_position.getColumn() < 9 && current_position.getRow() > 0 && current_position.getColumn() > 0
+            && board.getPiece(current_position) == null){
                 SingleMove = new ChessMove(startPosition, current_position, null);
                 forwardPossibilities.add(SingleMove) ;
             }
         }
-        overallPossibilities.addAll(promotionHelper(forwardPossibilities)) ;
+        overallPossibilities.addAll(promotionHelper(forwardPossibilities, end)) ;
         return overallPossibilities ;
         }
-    public Collection<ChessMove> promotionHelper(Collection<ChessMove> possibilities) {
+    public Collection<ChessMove> promotionHelper(Collection<ChessMove> possibilities, int end) {
         Collection<ChessMove> helperPossibilities = new ArrayList<>();
         for (ChessMove move : possibilities) {
-            if(move.getEndPosition().getRow() == 8){
+            if(move.getEndPosition().getRow() == end){
                 ChessMove SingleMove = new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.QUEEN) ;
                 helperPossibilities.add(SingleMove) ;
                 SingleMove = new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.ROOK) ;
@@ -54,7 +59,9 @@ public class PawnMovesCaculator implements PieceMovesCaculator {
                 helperPossibilities.add(SingleMove) ;
                 SingleMove = new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.BISHOP) ;
                 helperPossibilities.add(SingleMove) ;
-            } // fix for Black
+            } else {
+                helperPossibilities.add(move) ;
+            }
         }
         return helperPossibilities ;
     }
