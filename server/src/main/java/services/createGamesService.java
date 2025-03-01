9b5af4c27448891ebf8e509dd4e415a41f7d9528
mickbuck <mb2756@byu.edu>
2025-03-01 15:13:44 -0700

    Phase 3: Commit 8

--- server/src/main/java/services/createGamesService.java
+++ server/src/main/java/services/createGamesService.java
@@ -13,6 +13,9 @@ import responses.errors.UniqueError500;
 public class createGamesService {
     public static Integer create(String authToken, String gameName) throws BadRequest400, Unauthorized401, UniqueError500 {
         try {
+            if(authToken == null || gameName == null){
+                throw new BadRequest400() ;
+            }
             AuthDAO authDao = MemoryAuthDAO.getInstance();
             GameDAO gameDAO = MemoryGameDAO.getInstance();
             if (authDao.verifyAuth(authToken) == Boolean.TRUE) {
