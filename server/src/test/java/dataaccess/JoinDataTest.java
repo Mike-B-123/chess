c46141f54260ff788735b37890459d5dc6191036
mickbuck <mb2756@byu.edu>
2025-03-14 20:55:59 -0600

    Phase 4: Commit 17

--- server/src/test/java/dataaccess/JoinDataTest.java
+++ server/src/test/java/dataaccess/JoinDataTest.java
@@ -1,4 +1,55 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.Game;
+import model.JoinData;
+import model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import responses.errors.BadRequest400;
+import responses.errors.Taken403;
+import responses.errors.Unauthorized401;
+import responses.errors.UniqueError500;
+import services.ClearService;
+import services.CreateGamesService;
+import services.JoinGameService;
+import services.RegisterService;
+
 public class JoinDataTest {
+    @Test
+    public void positiveJoin() throws UniqueError500, BadRequest400, Taken403, Unauthorized401, DataAccessException {
+        ClearService.clear() ;
+        AuthDAO authDao = MySQLAuthDAO.getInstance() ;
+        UserDAO userDao = MySQLUserDAO.getInstance() ;
+        GameDAO gameDao = MySQLGameDAO.getInstance() ;
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        userDao.addUser(testUser);
+        AuthData authData = authDao.createAuth(testUser) ;
+        Game outputGame = gameDao.createGame("newTestGame") ;
+        JoinData joinData = new JoinData(ChessGame.TeamColor.BLACK, outputGame.gameID()) ;
+        gameDao.modifyInsert(authData.authToken(), joinData.playerColor(), joinData.gameID());
+        Assertions.assertEquals("testUserName", gameDao.getGame(joinData.gameID()).blackUsername());
+    }
+
+    @Test
+    public void negitiveJoin() throws UniqueError500, BadRequest400, Taken403, Unauthorized401, DataAccessException {
+        ClearService.clear() ;
+        AuthDAO authDao = MySQLAuthDAO.getInstance() ;
+        UserDAO userDao = MySQLUserDAO.getInstance() ;
+        GameDAO gameDao = MySQLGameDAO.getInstance() ;
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        userDao.addUser(testUser);
+        AuthData authData = authDao.createAuth(testUser) ;
+        Boolean exceptionThrown = Boolean.FALSE ;
+        try{
+            Game outPutErrorGame = CreateGamesService.create(authData.authToken(), "newTestGame") ;
+            JoinData testJoinData = new JoinData(ChessGame.TeamColor.BLACK, outPutErrorGame.gameID()) ;
+            gameDao.modifyInsert(null, testJoinData.playerColor(), 10000);
+        }
+        catch(Exception ex){ // double check that this was ok to look up
+            exceptionThrown = Boolean.TRUE ;
+        }
+        Assertions.assertEquals(Boolean.TRUE, exceptionThrown);
+    }
 }
--- server/src/test/java/dataaccess/RegisterDataTest.java
+++ server/src/test/java/dataaccess/RegisterDataTest.java
@@ -1,4 +1,44 @@
 package dataaccess;
 
+import model.AuthData;
+import model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import responses.errors.BadRequest400;
+import responses.errors.Taken403;
+import responses.errors.Unauthorized401;
+import responses.errors.UniqueError500;
+import services.ClearService;
+import services.RegisterService;
+
 public class RegisterDataTest {
+    @Test
+    public void positiveRegister() throws UniqueError500, BadRequest400, Taken403, Unauthorized401, DataAccessException {
+        ClearService.clear() ;
+        AuthDAO authDao = MySQLAuthDAO.getInstance() ;
+        UserDAO userDao = MySQLUserDAO.getInstance() ;
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        userDao.addUser(testUser);
+        AuthData authData = authDao.createAuth(testUser);
+        Assertions.assertEquals(authData.username(), testUser.username());
+        Assertions.assertNotNull(authData.authToken());
+    }
+
+    @Test
+    public void negitiveRegister() throws UniqueError500, BadRequest400, Taken403, DataAccessException {
+        ClearService.clear() ;
+        AuthDAO authDao = MySQLAuthDAO.getInstance() ;
+        UserDAO userDao = MySQLUserDAO.getInstance() ;
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        userDao.addUser(testUser);
+        Boolean exceptionThrown = Boolean.FALSE ;
+        try{
+            userDao.addUser(testUser);
+            authDao.createAuth(testUser);
+        }
+        catch(Exception ex){ // double check that this was ok to look up
+            exceptionThrown = Boolean.TRUE ;
+        }
+        Assertions.assertEquals(Boolean.TRUE, exceptionThrown);
+    }
 }
