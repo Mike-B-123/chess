8bd3e4dd73de3665e527ffaab3b4a7ca325154e3
mickbuck <mb2756@byu.edu>
2025-03-14 21:08:25 -0600

    Phase 4: Commit 18

--- server/src/test/java/dataaccess/ClearDataTest.java
+++ server/src/test/java/dataaccess/ClearDataTest.java
@@ -1,19 +1,41 @@
 package dataaccess;
 
 import model.AuthData;
+import model.Game;
 import model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import responses.errors.Unauthorized401;
 import responses.errors.UniqueError500;
 import services.ClearService;
 
 public class ClearDataTest {
-    String testClear = "{}";
 
     @Test
     public void positiveClearGame() throws UniqueError500, DataAccessException {
         GameDAO gameDao = MySQLGameDAO.getInstance();
-        Assertions.assertEquals(testClear, ClearService.clear().toString());
+        gameDao.clearGame();
+        Game testGame = gameDao.createGame("testGame") ;
+        gameDao.clearGame() ;
+        Assertions.assertEquals(null, gameDao.getGame(testGame.gameID()));
+    }
+    @Test
+    public void positiveClearUser() throws UniqueError500, DataAccessException, Unauthorized401 {
+        UserDAO userDao = MySQLUserDAO.getInstance();
+        userDao.clearUsers();
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        userDao.addUser(testUser);
+        userDao.clearUsers();
+        Assertions.assertEquals(null, userDao.findUser(testUser));
+    }
+    @Test
+    public void positiveClearAuth() throws UniqueError500, DataAccessException {
+        AuthDAO authDao = MySQLAuthDAO.getInstance();
+        authDao.clearAuths();
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        AuthData authData = authDao.createAuth(testUser) ;
+        authDao.clearAuths();
+        Assertions.assertEquals(null, authDao.getUsernameFromAuth(authData.authToken()));
     }
 }
 
--- server/src/test/java/dataaccess/CreateGameDataTest.java
+++ server/src/test/java/dataaccess/CreateGameDataTest.java
@@ -1,4 +1,48 @@
 package dataaccess;
 
+import model.AuthData;
+import model.Game;
+import model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import responses.errors.BadRequest400;
+import responses.errors.Taken403;
+import responses.errors.Unauthorized401;
+import responses.errors.UniqueError500;
+import services.ClearService;
+import services.CreateGamesService;
+import services.RegisterService;
+
 public class CreateGameDataTest {
+    @Test
+    public void positiveCreate() throws UniqueError500, BadRequest400, Taken403, Unauthorized401, DataAccessException {
+        ClearService.clear() ;
+        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+        AuthData authData = RegisterService.register(testUser) ;
+        Boolean test = Boolean.FALSE ;
+        Game outputGame = CreateGamesService.create(authData.authToken(), "newTestGame") ;
+        if(outputGame.gameID() > -1 ){
+            test = Boolean.TRUE ;
+        }
+        Assertions.assertEquals(Boolean.TRUE, test);
+    }
+
+    @Test
+    public void negitiveCreate() throws UniqueError500, BadRequest400, Taken403, Unauthorized401, DataAccessException {
+        ClearService.clear() ;
+//        AuthDAO authDao = MySQLAuthDAO.getInstance() ;
+//        UserDAO userDao = MySQLUserDAO.getInstance() ;
+        GameDAO gameDao = MySQLGameDAO.getInstance() ;
+//        User testUser = new User("testUserName", "testPassWord", "testEmail") ;
+//        userDao.addUser(testUser);
+//        authDao.createAuth(testUser) ;
+        Boolean exceptionThrown = Boolean.FALSE ;
+        try{
+            gameDao.createGame(null) ;
+        }
+        catch(Exception ex){ // double check that this was ok to look up
+            exceptionThrown = Boolean.TRUE ;
+        }
+        Assertions.assertEquals(Boolean.TRUE, exceptionThrown);
+    }
 }
