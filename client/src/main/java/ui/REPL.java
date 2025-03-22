5c1942ad43a7bcf8871f7f00a51f2cae6b9fd966
mickbuck <mb2756@byu.edu>
2025-03-22 13:35:20 -0600

    Phase 4: Commit 4

--- client/src/main/java/ui/REPL.java
+++ client/src/main/java/ui/REPL.java
@@ -1,9 +1,12 @@
 package ui;
 
+import java.util.Scanner;
+
 public class REPL {
     //private final JoinClient joinClient ;
     private final RegisterClient registerClient ;
     //private final BrowsingClient browsingClient ;
+    private State state = State.SIGNEDOUT ;
     public REPL(String serverURL){
         //joinClient = new JoinClient(serverURL, this) ;
         registerClient = new RegisterClient(serverURL) ;
@@ -11,6 +14,51 @@ public class REPL {
     }
     public void run(String serverURL){
         System.out.println("\uD83D\uDC36 Ready to play some Chess? First sign in.");
-        System.out.print(registerClient) ;
+        System.out.print(help()) ;
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        }
+
     }
+
+    public enum State{
+        SIGNEDOUT ,
+        SIGNEDIN
+    }
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    - signIn <yourname>
+                    - quit
+                    """;
+        }
+        return """
+                - list
+                - joinGame <gameID>
+                - rescue <name> <CAT|DOG|FROG|FISH>
+                - adoptAll
+                - signOut
+                - quit
+                """;
+    }
+public String eval(String input) {
+    try {
+        var tokens = input.toLowerCase().split(" ");
+        var cmd = (tokens.length > 0) ? tokens[0] : "help";
+        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        return switch (cmd) {
+            case "signin" -> signIn(params);
+            case "rescue" -> rescuePet(params);
+            case "list" -> listPets();
+            case "signout" -> signOut();
+            case "adopt" -> adoptPet(params);
+            case "adoptall" -> adoptAllPets();
+            case "quit" -> "quit";
+            default -> help();
+        };
+    } catch (ResponseException ex) {
+        return ex.getMessage();
+    }
+}
 }
--- client/src/main/java/ui/RegisterClient.java
+++ client/src/main/java/ui/RegisterClient.java
@@ -1,5 +1,8 @@
 package ui;
 import client.ServerFacade ;
+import java.io.*;
+import java.net.*;
+import ui.REPL.* ;
 
 public class RegisterClient {
     private final ServerFacade server;
