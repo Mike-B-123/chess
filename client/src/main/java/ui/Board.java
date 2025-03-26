3ef0269a6e329df19e6ebb233197a1b9d7c9153c
mickbuck <mb2756@byu.edu>
2025-03-26 12:44:22 -0600

    Phase 5: Commit 26 (working board with reverse)

--- client/src/main/java/ui/Board.java
+++ client/src/main/java/ui/Board.java
@@ -20,7 +20,7 @@ public class Board {
     private static final int[] rowHeaders = {8, 7, 6, 5, 4, 3, 2, 1};
     private static String squareColor = SET_BG_COLOR_WHITE;
     private static String[] headers = {"a", "b", "c", "d", "e", "f", "g", "h"};
-    private static String teamColor = "white"; // how do I fix the static aspect?
+    private static String teamColor = "black"; // how do I fix the static aspect?
     private static HashMap<ChessPiece.PieceType, String> pieceMap = new HashMap<>();
 
     public Board(String teamColor, String serverUrl) {
@@ -35,9 +35,6 @@ public class Board {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         setPieceMap();
         out.print(ERASE_SCREEN);
-        if(teamColor.equalsIgnoreCase("black")){
-            squareColor = SET_BG_COLOR_BLACK ;
-        }
 
         drawHeaders(out);
         drawChessBoard(out);
@@ -49,8 +46,7 @@ public class Board {
 
     private static void drawHeaders(PrintStream out) {
         out.print(SET_BG_COLOR_LIGHT_GREY);
-        out.print(SET_TEXT_COLOR_LIGHT_GREY);
-        out.print("1");
+        out.print(" ");
         if (teamColor.equalsIgnoreCase("white")) {
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
                 drawHeader(out, headers[boardCol]);
@@ -60,6 +56,8 @@ public class Board {
                 drawHeader(out, headers[boardCol]);
             }
         }
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(" ");
         out.print(SET_BG_COLOR_BLACK);
         out.println();
     }
@@ -82,11 +80,11 @@ public class Board {
 
     private static void drawChessBoard(PrintStream out) {
         out.print(SET_BG_COLOR_LIGHT_GREY);
-        int rightInt = 7;
+        int rightInt = 0;
         int leftInt = 0;
         if (teamColor.equalsIgnoreCase("black")) {
             leftInt = 7;
-            rightInt = 0;
+            rightInt = 7;
         }
         for (int squareRow = 0; squareRow < 8; ++squareRow) {
             out.print(SET_TEXT_COLOR_WHITE);
@@ -99,7 +97,12 @@ public class Board {
                     int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
                     int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
                     out.print(EMPTY.repeat(prefixLength));
-                    printPiece(squareRow, positionCol, out);
+                    if(teamColor.equalsIgnoreCase("white")){
+                    printWhitePiece(squareRow, positionCol, out);
+                    }
+                    else{
+                        printBlackPiece(squareRow, positionCol, out);
+                    }
                     out.print(EMPTY.repeat(suffixLength));
 
                 if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
@@ -118,11 +121,9 @@ public class Board {
     }
 
     private static void drawFooter(PrintStream out) {
-        out.print(SET_BG_COLOR_DARK_GREY);
         out.print(SET_BG_COLOR_LIGHT_GREY);
-        out.print(SET_TEXT_COLOR_LIGHT_GREY);
-        out.print("1");
-        if (teamColor.equalsIgnoreCase("black")) {
+        out.print(" ");
+        if (teamColor.equalsIgnoreCase("white")) {
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
                 drawHeader(out, headers[boardCol]);
             }
@@ -132,13 +133,33 @@ public class Board {
                 drawHeader(out, headers[boardCol]);
             }
         }
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(" ");
         out.print(SET_BG_COLOR_BLACK);
         out.println();
     }
-    private static void printPiece(int positionRow , int positionCol, PrintStream out) {
+    private static void printWhitePiece(int positionRow , int positionCol, PrintStream out) {
         positionCol += 1;
         positionRow += 1;
         ChessPiece piece = server.getCurrentChessBoard().getPiece(new ChessPosition(positionRow, positionCol));
+        if (piece != null) {
+            if (piece.getTeamColor().equals(ChessGame.TeamColor.BLACK)) {
+                out.print(SET_TEXT_COLOR_BLUE);
+                out.print(pieceMap.get(piece.getPieceType()));
+            } else {
+                out.print(SET_TEXT_COLOR_RED);
+                out.print(pieceMap.get(piece.getPieceType()));
+            }
+        } else {
+            out.print(" ");
+        }
+    }
+    private static void printBlackPiece(int positionRow , int positionCol, PrintStream out) {
+        positionCol += 1;
+        positionRow += 1;
+        int trueCol = 9 - positionCol;
+        int trueRow = 9 - positionRow ;
+        ChessPiece piece = server.getCurrentChessBoard().getPiece(new ChessPosition(trueRow, trueCol));
         if (piece != null) {
             if (piece.getTeamColor().equals(ChessGame.TeamColor.BLACK)) {
                 out.print(SET_TEXT_COLOR_BLUE);
@@ -173,10 +194,10 @@ public class Board {
 
     private static int rightIntMath(int rightInt){
         if(teamColor.equalsIgnoreCase("white")){
-            rightInt -- ;
+            rightInt ++ ;
         }
         else{
-            rightInt ++ ;
+            rightInt -- ;
         }
         return rightInt ;
     }
