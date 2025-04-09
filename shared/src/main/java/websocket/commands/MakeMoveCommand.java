fe5a9bb93d0f6f2a620f3e82735828aa316b4e2f
mickbuck <mb2756@byu.edu>
2025-04-09 14:37:46 -0600

    Phase 6: Commit 7

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,4 +1,13 @@
 package websocket.commands;
 
+import chess.ChessMove;
+
 public class MakeMoveCommand {
+    private ChessMove move ;
+    public MakeMoveCommand(ChessMove move){
+        this.move = move ;
+    }
+    public ChessMove getMove() {
+        return move ;
+    }
 }
